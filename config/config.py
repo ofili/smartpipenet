6715486e106d24a2010866fceb1555e4fd005c05
Ofili Lewis <ofililewis@gmail.com>
2023-05-16 17:54:08 +0100

    update config

---
+++ config/config.py
@@ -0,0 +1,17 @@
+PROJECT_ID = 'project-name'
+FIRESTORE_PROJECT_ID = ''
+FIRESTORE_COLLECTION_NAME = ''
+JOB_NAME = ''
+SUBSCRIPTION_ID = 'subscription-name'
+TOPIC_NAME = ''
+MQTT_BROKER_URL = ''
+MQTT_TOPIC = ''
+CASSANDRA_HOSTS = 'localhost'
+CASSANDRA_KEYSPACE = 'power_plants'
+CASSANDRA_TABLE = 'iot_sensor'
+INSTANCE_ID = ''
+TABLE_ID = ''
+DATASET_ID = ''
+COLUMN_FAMILY_ID = ''
+
+NUM_WORKERS = 8
